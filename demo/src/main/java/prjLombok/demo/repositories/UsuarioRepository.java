5564ae50d3835e16a86e292dd626d197ff5473b9
paol4zchs <133895456+paol4zchs@users.noreply.github.com>
2023-11-13 12:04:31 -0300

    Delete demo directory

--- demo/src/main/java/prjLombok/demo/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package prjLombok.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import prjLombok.demo.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
